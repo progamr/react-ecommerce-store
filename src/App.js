ff05b9284ff979bcd8f544a419b51f96d95294d0
Amr Mohamed <amr.mohamed@garonz.com>
2019-08-29 13:30:58 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+      <React.Fragment>
+          <h3>Hello from app</h3>
+      </React.Fragment>
+  );
+}
+
+export default App;
